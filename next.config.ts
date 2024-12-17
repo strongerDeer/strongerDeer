035cb7b70a0f59c75f0592dacfba61a7ddc04527
strongerDeer <stronger_deer@naver.com>
2024-12-17 21:02:53 +0900

    📝 Edit: assetPrefix 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/strongerDeer",
+  assetPrefix: "/strongerDeer",
   output: "export",
   images: {
     unoptimized: true,
