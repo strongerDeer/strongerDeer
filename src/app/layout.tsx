842fae461541f4a8269e492a54e5ca4ecc1a3994
strongerDeer <stronger_deer@naver.com>
2025-01-06 23:46:55 +0900

    📝 Edit: 경로수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,14 +18,14 @@ export const metadata: Metadata = {
       "0.3초의 짧은 인터랙션, 디테일한 사용자 경험을 고민하는 강혜진 개발자입니다.",
     locale: "ko_KR",
     siteName: "강혜진 | 프론트엔드 개발자",
-    images: ["/og-image.jpg"],
+    images: ["/strongerDeer/og-image.jpg"],
   },
   twitter: {
     card: "summary",
     title: "강혜진 | 프론트엔드 개발자",
     description:
       "0.3초의 짧은 인터랙션, 디테일한 사용자 경험을 고민하는 강혜진 개발자입니다.",
-    images: ["/og-image.jpg"],
+    images: ["/strongerDeer/og-image.jpg"],
   },
 };
 
